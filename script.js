478e46c7649f8b7757cafdb0c90f3a08072e7946
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-16 16:44:40 +0300

    add: complete task

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+class First {
+    constructor() {
+
+    }
+    hello() {
+        console.log("Привет, я метод родителя");
+    }
+}
+
+class Second extends First {
+    constructor() {
+        super();
+        
+    }
+    hello() {
+        console.log("А я наследуемый метод!");
+    }
+}
+const say = new Second();
+say.__proto__.__proto__.hello();
+say.hello();
